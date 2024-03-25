14b0b52ea6c5d195b39b42cf6a01619d4996f6ef
trusasha <hts.sasha800i@gmail.com>
2024-03-25 22:07:00 +0100

    fix(button): fixed component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,44 +1,24 @@
 import Text from 'components/text';
 import React, { FC } from 'react';
-import { TouchableOpacityProps, View, StyleSheet, TouchableOpacity } from 'react-native';
-import Animated, { useAnimatedStyle, useDerivedValue, withTiming } from 'react-native-reanimated';
+import { TouchableOpacityProps, StyleSheet, TouchableOpacity } from 'react-native';
 import { theme } from 'theme';
-
-const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
 
 interface Props extends Omit<TouchableOpacityProps, 'disabled'> {
   label: string;
   isDisabled?: boolean;
 }
 
-const Button: FC<Props> = ({ label, isDisabled, ...rest }) => {  
-  const disabledValue = useDerivedValue(() => withTiming(isDisabled ? 0.5 : 1), [isDisabled]);
-
-  const animatedButtonStyle = useAnimatedStyle(
-    () => ({
-      opacity: disabledValue.value,
-    }),
-    [isDisabled],
-  );
-
-  return (
-    <View pointerEvents={isDisabled ? 'none' : 'auto'} style={styles.buttonContainer}>
-      <AnimatedTouchable {...rest} style={[styles.button, rest.style, animatedButtonStyle]}>
-        <Text fontSize={theme.fontSizes.xl} color={theme.colors.planeWhite} fontWeight="600">
-          {label}
-        </Text>
-      </AnimatedTouchable>
-    </View>
-  );
-};
+const Button: FC<Props> = ({ label, isDisabled, ...rest }) => (
+  <TouchableOpacity {...rest} style={[styles.button, rest.style, { opacity: isDisabled ? 0.5 : 1 }]}>
+    <Text fontSize={theme.fontSizes.xl} color={theme.colors.planeWhite} fontWeight="600">
+      {label}
+    </Text>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    height: 80,
-    width: '100%',
-  },
   button: {
-    flex: 1,
+    height: 48,
     backgroundColor: '#111',
     margin: theme.spacings.s,
     borderRadius: theme.borderRadiuses.s,
