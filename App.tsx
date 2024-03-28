d77a59fb57e4a4777e767ac166d971fbe6409973
trusasha <hts.sasha800i@gmail.com>
2024-03-28 22:12:44 +0100

    added rersisted navigation state

--- App.tsx
+++ App.tsx
@@ -1,16 +1,52 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MainStack from 'navigation/main-stack';
 import { ThemeProvider } from 'theme';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
+import { Linking, Platform, StyleSheet, View } from 'react-native';
 import Toast from 'react-native-toast-message';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const PERSISTENCE_KEY = 'NAVIGATION_STATE_V1';
 
 function App(): React.JSX.Element {
+  const [isReady, setIsReady] = useState(Platform.OS === 'web'); // Don't persist state on web since it's based on URL
+  const [initialState, setInitialState] = useState();
+
+  useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (initialUrl == null) {
+          const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+          const state = savedStateString ? JSON.parse(savedStateString) : undefined;
+
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return <View />;
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      initialState={initialState}
+      onStateChange={state => AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))}
+    >
       <SafeAreaProvider>
         <GestureHandlerRootView style={styles.flex}>
           <ThemeProvider>
